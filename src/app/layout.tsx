52b4490c2ebe17392ea26004e67aa3e497d6f1ef
Shaun Jhingoor <Jhingoor1945@gmail.com>
2024-10-07 18:16:40 -0400

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-6SL2J917ZS"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-6SL2J917ZS');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
