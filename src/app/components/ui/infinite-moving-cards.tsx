5c74cdb13abad780d248f5948e9144bd56717d36
Shaun Jhingoor <Jhingoor1945@gmail.com>
2025-10-28 22:31:44 -0400

    add new resume

--- src/app/components/ui/infinite-moving-cards.tsx
+++ src/app/components/ui/infinite-moving-cards.tsx
@@ -60,7 +60,7 @@ export const InfiniteMovingCards = ({
   const getSpeed = () => {
     if (containerRef.current) {
       if (speed === "fast") {
-        containerRef.current.style.setProperty("--animation-duration", "20s");
+        containerRef.current.style.setProperty("--animation-duration", "5s");
       } else if (speed === "normal") {
         containerRef.current.style.setProperty("--animation-duration", "40s");
       } else {
