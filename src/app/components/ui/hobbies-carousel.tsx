32ae94df8a8b06c378ef0a5c711ae17fab1e0501
Shaun Jhingoor <Jhingoor1945@gmail.com>
2025-11-15 00:25:01 -0500

    Current book

--- src/app/components/ui/hobbies-carousel.tsx
+++ src/app/components/ui/hobbies-carousel.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { useState } from 'react';
-import Image from 'next/image';
-import styles from './Carousel.module.css'; 
-import reading from "../../assets/reading.png"
-import hiking from "../../assets/Hiking.png"
-import car from "../../assets/cars.png"
+import { useState } from "react";
+import Image from "next/image";
+import styles from "./Carousel.module.css";
+import reading from "../../assets/reading.png";
+import hiking from "../../assets/Hiking.png";
+import car from "../../assets/cars.png";
 
 interface CarouselItem {
   imgSrc: string;
@@ -19,11 +19,13 @@ const Carousel: React.FC = () => {
   const items: CarouselItem[] = [
     {
       imgSrc: reading.src,
-      alt: 'reading',
-      title: 'Reading',
+      alt: "reading",
+      title: "Reading",
       description: (
         <>
-          I love diving into a good book. I am currently reading <i>All The Dangerous Things</i> by Stacy Willingham. Happy to talk about a good book anytime. My{' '}
+          I love diving into a good book. I am currently reading{" "}
+          <i>Rich Dad Poor Dad</i> by Robert Kiyosaki and Sharon Lechter. Happy
+          to talk about a good book anytime. My{" "}
           <a
             href="https://github.com/ShaunJhingoor"
             aria-label="github"
@@ -32,18 +34,20 @@ const Carousel: React.FC = () => {
             rel="noopener noreferrer"
           >
             <span>GitHub</span>
-          </a>{' '}
+          </a>{" "}
           contains a list of a few of my favorite books.
         </>
       ),
     },
     {
       imgSrc: hiking.src,
-      alt: 'hiking',
-      title: 'Hiking',
+      alt: "hiking",
+      title: "Hiking",
       description: (
         <>
-          I love getting out into nature and exploring new trails. Whenever I get stuck on a coding problem, getting outside and getting some sun is a great reboot for my mind. Check out{' '}
+          I love getting out into nature and exploring new trails. Whenever I
+          get stuck on a coding problem, getting outside and getting some sun is
+          a great reboot for my mind. Check out{" "}
           <a
             href="https://alladventure.onrender.com/"
             aria-label="adventure"
@@ -52,18 +56,20 @@ const Carousel: React.FC = () => {
             rel="noopener noreferrer"
           >
             All Adventure
-          </a>{' '}
+          </a>{" "}
           to see some great trails!
         </>
       ),
     },
     {
       imgSrc: car.src,
-      alt: 'cars',
-      title: 'Working on cars',
+      alt: "cars",
+      title: "Working on cars",
       description: (
         <>
-          I love diving into new puzzles and putting them together. Coding makes sense since I have been troubleshooting cars most of my life. Like coding, working on cars is another puzzle I love.
+          I love diving into new puzzles and putting them together. Coding makes
+          sense since I have been troubleshooting cars most of my life. Like
+          coding, working on cars is another puzzle I love.
         </>
       ),
     },
@@ -83,22 +89,39 @@ const Carousel: React.FC = () => {
 
   return (
     <section id="hobbies">
-      <h2 className="sectionTitle text-4xl md:text-5xl font-bold mb-8 text-center text-white pb-4 drop-shadow-lg">Hobbies</h2>
+      <h2 className="sectionTitle text-4xl md:text-5xl font-bold mb-8 text-center text-white pb-4 drop-shadow-lg">
+        Hobbies
+      </h2>
       <div className={styles.carouselContainer}>
-        <div className={styles.carousel} style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+        <div
+          className={styles.carousel}
+          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+        >
           {items.map((item, index) => (
             <div key={index} className={styles.carouselItem}>
-              <Image src={item.imgSrc} alt={item.alt} width={60} height={60} id={styles.imageHobbies}/>
+              <Image
+                src={item.imgSrc}
+                alt={item.alt}
+                width={60}
+                height={60}
+                id={styles.imageHobbies}
+              />
               <br />
               <h3 id={styles.hobbiesTitle}>{item.title}</h3>
               <p id={styles.hobbiesdescription}>{item.description}</p>
             </div>
           ))}
         </div>
-        <button className={`${styles.carouselControl} ${styles.prev}`} onClick={handlePrev}>
+        <button
+          className={`${styles.carouselControl} ${styles.prev}`}
+          onClick={handlePrev}
+        >
           &#10094;
         </button>
-        <button className={`${styles.carouselControl} ${styles.next}`} onClick={handleNext}>
+        <button
+          className={`${styles.carouselControl} ${styles.next}`}
+          onClick={handleNext}
+        >
           &#10095;
         </button>
       </div>
